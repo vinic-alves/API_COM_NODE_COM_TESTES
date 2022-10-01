dd6f8fd76bd215e4505d302c14715c7e11fe536b
vinic-alves <vinicius.alves95@outlook.com>
2022-10-01 18:39:19 -0300

    testando se o servidor está rodando na porta 3001

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,21 @@
+const supertest = require('supertest')
+
+const request = supertest('http://localhost:3001');
+
+
+
+
 test('Deve responder na porta 3001', ()=>{
     // Acessar a Url http://localhost:3001
+      return request.get('/')
+      .then(res => expect(res.status).toBe(200))
+
+
+
+
+
+
     // Verificar que a resposta  foi 200
 
-    
+
 }) 
