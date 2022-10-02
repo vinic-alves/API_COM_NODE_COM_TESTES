59ff0e7fd32ba1f6135eb292d523f054ca0cf8d3
vinic-alves <vinicius.alves95@outlook.com>
2022-10-02 13:40:35 -0300

    Secure-mode

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,12 @@
  // O Teste será: Deve responder a raiz 
 
-const { request } = require("express");
-const supertest = require("supertest");
+const request = require("supertest");
 
 const app = require('../src/app')
 
 test('Deve responder na a raiz', ()=>{
     return request(app).get('/')
     .then((res)=> {
-        expect(res.status).toBe(200);
+        expect(res.status).toBe(400);
     })
 })
